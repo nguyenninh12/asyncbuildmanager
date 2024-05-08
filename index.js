5e069c01ec007accdfc7958d79c18a6571bfd1b9
nguyenninh12 <b.petrusmanam1311@outlook.com>
2024-05-08 18:09:10 +0700

    start commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function countWords(str) {
-  return str.split(" ").filter((word) => word !== "").length;
-}
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
+};
