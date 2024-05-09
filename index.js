63710bfdb7ac0ec9f37affbf38ca82a1aa5fa447
nguyenninh12 <b.petrusmanam1311@outlook.com>
2024-05-09 08:07:24 +0700

    feat task

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const difference = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => !s.has(x));
-};
+// Alice should have the initial balance minus tranfer amount
+if (aliceBalanceAfter[0].amount !== ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT)
+  throw new Error("Alice's balance after transfer is incorrect");
+
+// Sponsor should have the initial balance minus gas
+if (sponsorBalanceAfter[0].amount >= SPONSOR_INITIAL_BALANCE)
+  throw new Error("Sponsor's balance after transfer is incorrect");
