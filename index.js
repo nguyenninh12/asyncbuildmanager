161dc5e8bdcc3cb4c934460669d232e8a6281dfc
nguyenninh12 <b.petrusmanam1311@outlook.com>
2024-05-12 06:05:42 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  if (obstacleGrid[0][0] === 1) return 0;
-  dp[0][0] = 1;
-  for (let i = 1; i < m; i++) {
-    if (obstacleGrid[i][0] === 0) dp[i][0] = dp[i - 1][0];
-  }
-  for (let j = 1; j < n; j++) {
-    if (obstacleGrid[0][j] === 0) dp[0][j] = dp[0][j - 1];
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      if (obstacleGrid[i][j] === 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
+function buildTree(preorder, inorder) {
+  if (preorder.length === 0 || inorder.length === 0) return null;
+  const rootVal = preorder[0];
+  const root = new TreeNode(rootVal);
+  const index = inorder.indexOf(rootVal);
+  const leftInorder = inorder.slice(0, index);
+  const rightInorder = inorder.slice(index + 1);
+  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
+  const rightPreorder = preorder.slice(1 + leftInorder.length);
+  root.left = buildTree(leftPreorder, leftInorder);
+  root.right = buildTree(rightPreorder, rightInorder);
+  return root;
 }
