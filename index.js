3c9b267d54d8110efe7e83946ddfe1afc9fa8e0e
nguyenninh12 <b.petrusmanam1311@outlook.com>
2024-05-13 11:12:25 +0700

    start project

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-function lowestCommonAncestor(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  const left = lowestCommonAncestor(root.left, p, q);
-  const right = lowestCommonAncestor(root.right, p, q);
-  if (left && right) return root;
-  return left ? left : right;
-}
+const currentDate = new Date();
+console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
